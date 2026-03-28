435b1c74f9712247376dc1bd851ad3824b40deee
Samircrypto67 <diampakaisaac@gmail.com>
2026-03-28 12:04:17 +0100

    modification root
    -Créer update(Employe employe) dans GestionPersonnel
    -Modifier les setters dans Employe pour appeler gestionPersonnel.update(this)

--- Personnel/src/personnel/Employe.java
+++ Personnel/src/personnel/Employe.java
@@ -2,64 +2,64 @@ package personnel;
 
 import java.io.Serializable;
 
-public class Employe implements Serializable, Comparable<Employe>
-{
+public class Employe implements Serializable, Comparable<Employe> {
+
     private static final long serialVersionUID = 4795721718037994734L;
-    
+
     private int id = -1; // id pour la BDD
     private String nom, prenom, password, mail;
+    private java.sql.Date dateArrivee, dateDepart;
     private Ligue ligue;
     private GestionPersonnel gestionPersonnel;
 
-    // Constructeur normal pour créer un nouvel employé
-    Employe(GestionPersonnel gestionPersonnel, Ligue ligue, String nom, String prenom, String mail, String password)
-    {
+    // Constructeur normal
+    public Employe(GestionPersonnel gestionPersonnel, Ligue ligue, String nom, String prenom,
+                   String mail, String password, java.sql.Date dateArrivee, java.sql.Date dateDepart) {
         this.gestionPersonnel = gestionPersonnel;
         this.ligue = ligue;
         this.nom = nom;
         this.prenom = prenom;
         this.mail = mail;
         this.password = password;
+        this.dateArrivee = dateArrivee;
+        this.dateDepart = dateDepart;
     }
 
-    //  Constructeur pour charger depuis BDD 
-    Employe(GestionPersonnel gestionPersonnel, Ligue ligue, int id, String nom, String prenom, String mail, String password)
-    {
-        this.gestionPersonnel = gestionPersonnel;
-        this.ligue = ligue;
+    // Constructeur pour charger depuis BDD
+    public Employe(GestionPersonnel gestionPersonnel, Ligue ligue, int id, String nom, String prenom,
+                   String mail, String password, java.sql.Date dateArrivee, java.sql.Date dateDepart) {
+        this(gestionPersonnel, ligue, nom, prenom, mail, password, dateArrivee, dateDepart);
         this.id = id;
-        this.nom = nom;
-        this.prenom = prenom;
-        this.mail = mail;
-        this.password = password;
     }
 
-    // Getters et setters
+    // Ajout de Getters et setters avec update automatique.
     public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
     public String getNom() { return nom; }
-    public void setNom(String nom) { this.nom = nom; }
+    public void setNom(String nom) { this.nom = nom; try { gestionPersonnel.update(this); } catch (Exception e) {} }
     public String getPrenom() { return prenom; }
-    public void setPrenom(String prenom) { this.prenom = prenom; }
+    public void setPrenom(String prenom) { this.prenom = prenom; try { gestionPersonnel.update(this); } catch (Exception e) {} }
     public String getMail() { return mail; }
-    public void setMail(String mail) { this.mail = mail; }
+    public void setMail(String mail) { this.mail = mail; try { gestionPersonnel.update(this); } catch (Exception e) {} }
     public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public void setPassword(String password) { this.password = password; try { gestionPersonnel.update(this); } catch (Exception e) {} }
     public Ligue getLigue() { return ligue; }
+    public void setLigue(Ligue ligue) { this.ligue = ligue; try { gestionPersonnel.update(this); } catch (Exception e) {} }
+    public java.sql.Date getDateArrivee() { return dateArrivee; }
+    public void setDateArrivee(java.sql.Date dateArrivee) { this.dateArrivee = dateArrivee; try { gestionPersonnel.update(this); } catch (Exception e) {} }
+    public java.sql.Date getDateDepart() { return dateDepart; }
+    public void setDateDepart(java.sql.Date dateDepart) { this.dateDepart = dateDepart; try { gestionPersonnel.update(this); } catch (Exception e) {} }
 
     public boolean estRoot() { return gestionPersonnel.getRoot() == this; }
 
     @Override
-    public int compareTo(Employe autre)
-    {
+    public int compareTo(Employe autre) {
         int cmp = getNom().compareTo(autre.getNom());
         if (cmp != 0) return cmp;
         return getPrenom().compareTo(autre.getPrenom());
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         String res = nom + " " + prenom + " " + mail + " (";
         if (estRoot())
             res += "super-utilisateur";
